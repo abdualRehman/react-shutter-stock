87062a4772822cc8bddd0e4de26f6d5b2956c612
Rehman ali <rehmanali00708@gmail.com>
2020-04-01 20:04:32 +0500

    adding cors permission for download images

--- src/components/PhotoDetails.js
+++ src/components/PhotoDetails.js
@@ -9,6 +9,9 @@ import swal from 'sweetalert';
 
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { GalleryContext } from '../context/GalleryContext';
+import { UserContext } from '../context/UserContext';
+
+
 
 class PhotoDetails extends React.Component {
 
@@ -20,6 +23,7 @@ class PhotoDetails extends React.Component {
             details: {},
             height: '',
             width: '',
+            creatorDetails: {},
         }
     }
 
@@ -63,6 +67,7 @@ class PhotoDetails extends React.Component {
 
         if (width === '' || height === '') {
 
+
             var xhr = new XMLHttpRequest();//
             xhr.open("GET", url, true);
             xhr.responseType = "blob";//
@@ -77,6 +82,8 @@ class PhotoDetails extends React.Component {
                 document.body.removeChild(tag);
             }
             xhr.send();
+
+
 
         } else {
 
@@ -141,88 +148,100 @@ class PhotoDetails extends React.Component {
 
 
     }
+    findName = (userContext) => {
+
+        if (userContext.users.length !== 0) {
+            var id = this.state.details.user_id;
+            var data = userContext.findById(id);
+            return data.email
+        }
+
+    }
 
     render() {
         const images = [{ src: this.state.details.src }]
         return (
             <GalleryContext.Consumer>
                 {(gallery) => {
-
                     return (
-                        <div>
-                            <Header2 />
+                        <UserContext.Consumer>
+                            {(userContext) => {
 
-                            <div className="container">
-                                <div className="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
-                                    <Link to="/" className="stext-109 cl8 hov-cl1 trans-04">
-                                        Home
-				                    <i className="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
-                                    </Link>
-                                    <Link to="/images" className="stext-109 cl8 hov-cl1 trans-04">
-                                        Images
-				                    <i className="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
-                                    </Link>
-                                    <span className="stext-109 cl4">
-                                        Photo Details
-			                        </span>
-                                </div>
-                            </div>
-                            <div className="container">
-                                <div className="row">
+                                return (
+                                    <div>
+                                        <Header2 />
 
-                                    <div className="col-md-7">
-                                        <div className="details-image">
-                                            {/* {gallery.photos.find(this.findDetails)} */}
-                                            <img src={this.state.details.src} alt="imagedetails" />
-
-                                            <button onClick={this.openLightbox} ><i className="fas 3x fa-binoculars"></i></button>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-5">
                                         <div className="container">
-                                            <div className="detailSectionSinup">
-                                                <div>
-                                                    <div>
-                                                        Created by: {this.state.details.user_id}
+                                            <div className="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
+                                                <Link to="/" className="stext-109 cl8 hov-cl1 trans-04">
+                                                    Home
+				                    <i className="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
+                                                </Link>
+                                                <Link to="/images" className="stext-109 cl8 hov-cl1 trans-04">
+                                                    Images
+				                    <i className="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
+                                                </Link>
+                                                <span className="stext-109 cl4">
+                                                    Photo Details
+			                        </span>
+                                            </div>
+                                        </div>
+                                        <div className="container">
+                                            <div className="row">
+
+                                                <div className="col-md-7">
+                                                    <div className="details-image">
+                                                        {/* {gallery.photos.find(this.findDetails)} */}
+                                                        <img src={this.state.details.src} alt="imagedetails" />
+
+                                                        <button onClick={this.openLightbox} ><i className="fas 3x fa-binoculars"></i></button>
                                                     </div>
-                                                    <br /><br />
-                                                    <div>
-                                                        Price: {this.state.details.price_status ? this.state.details.price : "Free"}
-                                                    </div>
-                                                    <br />
-                                                    {/* <button className="btn green p-2" style={{width:"100%"}} onClick={this.addToCart} > <i className="fas fa-cart-plus"></i> Add To Cart</button><br /><br /> */}
-
-                                                    {this.state.details.price_status ? <button className="btn green p-2" style={{ width: "100%" }} onClick={this.addToCart} > <i className="fas fa-cart-plus"></i> Add To Cart</button> :
-                                                        <button className='dropdown-trigger btn downloadBtn green p-1' data-target='dropdown1'><i className="fas fa-download"></i> Download</button>
-                                                    }
-
-                                                    <ul id='dropdown1' style={{ width: "inherit" }} className='dropdown-content downloadContent'>
-                                                        <li>
-                                                            <label>
-                                                                <input className="with-gap" name="group1" defaultChecked onChange={this.toggleCustomBox} type="radio" />
-                                                                <span>Orignal Height and Width</span>
-                                                            </label>
-                                                        </li>
-                                                        <li>
-                                                            <label>
-                                                                <input className="with-gap" name="group1" onChange={this.toggleCustomBox} type="radio" />
-                                                                <span>Custom Height and Width</span>
-                                                            </label>
-                                                            {(this.state.customBoxIsOpen) ?
-                                                                <div className="customDownload">
-                                                                    <ul className="customDownload1">
-                                                                        <li style={{ width: "40%" }} ><input type="text" placeholder="width" name="width" onChange={this.handleChange} className="customWidth" /></li>
-                                                                        <li className="cross" style={{ width: "auto", padding: "30px 0px 0px 0px" }} >X</li>
-                                                                        <li style={{ width: "40%" }} ><input type="text" placeholder="height" name="height" onChange={this.handleChange} className="customWidth" /></li>
-                                                                    </ul>
-                                                                </div>
-                                                                : null}
-                                                        </li>
-
-                                                        <li><button className="btn confirmDownload green" style={{ color: "white", paddingTop: "7px", borderRadius: "15px", width: "100%" }} onClick={this.downloadImage} ><i className="fas fa-download"></i> Download</button></li>
-                                                    </ul>
                                                 </div>
-                                                {/* <div>
+                                                <div className="col-md-5">
+                                                    <div className="container">
+                                                        <div className="detailSectionSinup">
+                                                            <div>
+                                                                <div>
+                                                                    Created by: {this.findName(userContext)}
+                                                                </div>
+                                                                <br /><br />
+                                                                <div>
+                                                                    Price: {this.state.details.price_status ? this.state.details.price : "Free"}
+                                                                </div>
+                                                                <br />
+                                                                {/* <button className="btn green p-2" style={{width:"100%"}} onClick={this.addToCart} > <i className="fas fa-cart-plus"></i> Add To Cart</button><br /><br /> */}
+
+                                                                {this.state.details.price_status ? <button className="btn green p-2" style={{ width: "100%" }} onClick={this.addToCart} > <i className="fas fa-cart-plus"></i> Add To Cart</button> :
+                                                                    <button className='dropdown-trigger btn downloadBtn green p-1' data-target='dropdown1'><i className="fas fa-download"></i> Download</button>
+                                                                }
+
+                                                                <ul id='dropdown1' style={{ width: "inherit" }} className='dropdown-content downloadContent'>
+                                                                    <li>
+                                                                        <label>
+                                                                            <input className="with-gap" name="group1" defaultChecked onChange={this.toggleCustomBox} type="radio" />
+                                                                            <span>Orignal Height and Width</span>
+                                                                        </label>
+                                                                    </li>
+                                                                    <li>
+                                                                        <label>
+                                                                            <input className="with-gap" name="group1" onChange={this.toggleCustomBox} type="radio" />
+                                                                            <span>Custom Height and Width</span>
+                                                                        </label>
+                                                                        {(this.state.customBoxIsOpen) ?
+                                                                            <div className="customDownload">
+                                                                                <ul className="customDownload1">
+                                                                                    <li style={{ width: "40%" }} ><input type="text" placeholder="width" name="width" onChange={this.handleChange} className="customWidth" /></li>
+                                                                                    <li className="cross" style={{ width: "auto", padding: "30px 0px 0px 0px" }} >X</li>
+                                                                                    <li style={{ width: "40%" }} ><input type="text" placeholder="height" name="height" onChange={this.handleChange} className="customWidth" /></li>
+                                                                                </ul>
+                                                                            </div>
+                                                                            : null}
+                                                                    </li>
+
+                                                                    <li><button className="btn confirmDownload green" style={{ color: "white", paddingTop: "7px", borderRadius: "15px", width: "100%" }} onClick={this.downloadImage} ><i className="fas fa-download"></i> Download</button></li>
+                                                                </ul>
+                                                            </div>
+                                                            {/* <div>
                                                     <h2>Sign up</h2>
                                                     <div className="container">
                                                         <div className="input-field col-sm-12">
@@ -245,32 +264,36 @@ class PhotoDetails extends React.Component {
                                                     </div>
                                                 </div>
                                             */}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-md-12">
+                                                    <h1>{this.state.details.title}</h1>
+                                                    <br />
+                                                    <p>{this.state.details.description}</p>
+                                                    <br />
+                                                </div>
                                             </div>
                                         </div>
+                                        <ModalGateway>
+                                            {this.state.viewerIsOpen ? (
+                                                <Modal onClose={this.closeLightbox}>
+                                                    <Carousel views={images} />
+                                                </Modal>
+                                            ) : null}
+                                        </ModalGateway>
+
+
+
+
+                                        <Footer />
                                     </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <h1>{this.state.details.title}</h1>
-                                        <br />
-                                        <p>{this.state.details.description}</p>
-                                        <br />
-                                    </div>
-                                </div>
-                            </div>
-                            <ModalGateway>
-                                {this.state.viewerIsOpen ? (
-                                    <Modal onClose={this.closeLightbox}>
-                                        <Carousel views={images} />
-                                    </Modal>
-                                ) : null}
-                            </ModalGateway>
+                                )
+                            }}
 
-
-
-
-                            <Footer />
-                        </div>
+                        </UserContext.Consumer>
                     )
                 }}
             </GalleryContext.Consumer>
